853166153d6293aef87c81fc4e8d096eecfa6ca5
GH-Li <liguohao010701@qq.com>
2022-05-28 21:43:39 +0800

    修改部分界面情况
    下一步采用xml的data标签，使用databinding进行数据列表实现

--- app/src/main/java/com/example/bookmall/activity/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/bookmall/activity/ui/home/HomeFragment.java
@@ -4,28 +4,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.bookmall.R;
 import com.example.bookmall.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private ListView categoryListView;
+    private ListView bookListView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, null);
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-//        final TextView textView = binding.textHome;
-//        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
     }
 
     @Override
