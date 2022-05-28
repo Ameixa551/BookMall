3687cc4f4bbe5ed1e61088057b502fd72babba74
GH-Li <liguohao010701@qq.com>
2022-05-28 20:36:40 +0800

    完成二次无感登录功能

--- app/src/main/java/com/example/bookmall/MainActivity.java
+++ app/src/main/java/com/example/bookmall/MainActivity.java
@@ -2,42 +2,53 @@ package com.example.bookmall;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.bookmall.SQLiteDB.MyDataBaseHelper;
 import com.example.bookmall.activity.LoginActivity;
+import com.example.bookmall.activity.MallActivity;
 import com.example.bookmall.activity.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button login;
-    private Button register;
+    private final MyDataBaseHelper myDataBaseHelper = new MyDataBaseHelper(MainActivity.this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        login = findViewById(R.id.main_login);
-        register = findViewById(R.id.main_register);
+        Button login = findViewById(R.id.main_login);
+        Button register = findViewById(R.id.main_register);
+        SharedPreferences sharedPreferences = getSharedPreferences("userInfo",
+                Context.MODE_PRIVATE);
+        String name = sharedPreferences.getString("name", null);
+        String pwd = sharedPreferences.getString("pwd", null);
 
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-                startActivity(intent);
-                finish();
-            }
+        SQLiteDatabase db = myDataBaseHelper.getReadableDatabase();
+
+        // 如果不为空，则直接验证登录
+        if (name != null && pwd != null && myDataBaseHelper.isUserExist(db, name, pwd)) {
+            Toast.makeText(MainActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, MallActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        register.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(intent);
+            finish();
         });
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent (MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
+        login.setOnClickListener(view -> {
+            Intent intent = new Intent (MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
         });
     }
 }
--- app/src/main/java/com/example/bookmall/activity/LoginActivity.java
+++ app/src/main/java/com/example/bookmall/activity/LoginActivity.java
@@ -2,63 +2,64 @@ package com.example.bookmall.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.bookmall.sqLiteDB.MyDataBaseHelper;
 import com.example.bookmall.R;
+import com.example.bookmall.SQLiteDB.MyDataBaseHelper;
 
 public class LoginActivity extends AppCompatActivity {
-    private Button login;
-    private TextView goRegister;
     private EditText username;
-    private EditText userpassword;
-    private MyDataBaseHelper myDataBaseHelper = new MyDataBaseHelper(LoginActivity.this);
+    private EditText userPassword;
+    private final MyDataBaseHelper myDataBaseHelper = new MyDataBaseHelper(LoginActivity.this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        login = findViewById(R.id.login_login_btn);
-        goRegister = findViewById(R.id.login_linkregister_txtview);
+        Button login = findViewById(R.id.login_login_btn);
+        TextView goRegister = findViewById(R.id.login_linkregister_txtview);
         username = findViewById(R.id.login_username_txt);
-        userpassword = findViewById(R.id.login_password_txt);
-        SQLiteDatabase db = myDataBaseHelper.getWritableDatabase();
+        userPassword = findViewById(R.id.login_password_txt);
+        SQLiteDatabase db = myDataBaseHelper.getReadableDatabase();
+        SharedPreferences sharedPreferences = getSharedPreferences("userInfo",
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String name = username.getText().toString().trim();
-                String password = userpassword.getText().toString().trim();
-                if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(password)) {
-                    if (myDataBaseHelper.isUserExist(db, name, password)) {
-                        Toast.makeText(LoginActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(LoginActivity.this,MallActivity.class);
-                        startActivity(intent);
-                        finish();
-                    } else {
-                        Toast.makeText(LoginActivity.this, "登录失败，请重试", Toast.LENGTH_SHORT).show();
-                    }
+        login.setOnClickListener(view -> {
+            String name = username.getText().toString().trim();
+            String password = userPassword.getText().toString().trim();
+            if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(password)) {
+                if (myDataBaseHelper.isUserExist(db, name, password)) {
+                    // 将用户信息添加至SharedPreference
+                    editor.putString("name", name);
+                    editor.putString("pwd", password);
+                    editor.apply();
 
+                    Toast.makeText(LoginActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this,MallActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "登录失败，请重试", Toast.LENGTH_SHORT).show();
                 }
+
             }
         });
 
-        goRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
-                startActivity(intent);
-                finish();
-            }
+        goRegister.setOnClickListener(view -> {
+            Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+            startActivity(intent);
+            finish();
         });
     }
 }
