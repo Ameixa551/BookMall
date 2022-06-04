e0dad2bc4ecb1565d6c00ade483666d332a61935
GH-Li <liguohao010701@qq.com>
2022-06-05 03:47:21 +0800

    详情跳转完成

--- app/src/main/java/com/example/bookmall/activity/HistoryOrderActivity.java
+++ app/src/main/java/com/example/bookmall/activity/HistoryOrderActivity.java
@@ -1,11 +1,14 @@
 package com.example.bookmall.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.example.bookmall.R;
+import com.example.bookmall.databinding.ActivityHistoryOrderBinding;
+import com.example.bookmall.models.DisplayOrder;
 
 public class HistoryOrderActivity extends AppCompatActivity {
 
@@ -13,9 +16,12 @@ public class HistoryOrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history_order);
-
+        ActivityHistoryOrderBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_history_order);
         //返回按钮
         ImageView goBack = findViewById(R.id.history_order_back);
         goBack.setOnClickListener(view -> finish());
+
+        DisplayOrder order = (DisplayOrder) getIntent().getSerializableExtra("order");
+        binding.setOrder(order);
     }
 }
